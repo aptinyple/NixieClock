19db2f792cdf38f96d13d5b0f84a64acd14f7f6e
¨aptinyple¨ <ris92@mail.ru>
2015-10-01 02:58:14 +0600

    Add PowerBoard pcb

---
+++ Schematic/PowerBoard/PowerBoard.cmd
@@ -0,0 +1,97 @@
+# Pin name action command file
+
+# Start of element J3
+ChangePinName(J3, 3, 3)
+ChangePinName(J3, 2, 2)
+ChangePinName(J3, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element VD1
+ChangePinName(VD1, 3, -)
+ChangePinName(VD1, 4, +)
+ChangePinName(VD1, 2, ~)
+ChangePinName(VD1, 1, ~)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
+
+# Start of element F1
+ChangePinName(F1, 1, 1)
+ChangePinName(F1, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 3, 3)
+ChangePinName(U1, 2, 2)
+ChangePinName(U1, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 5, COMP)
+ChangePinName(U2, 6, VCC)
+ChangePinName(U2, 7, IPK)
+ChangePinName(U2, 8, COLL_D)
+ChangePinName(U2, 4, GND)
+ChangePinName(U2, 3, CT)
+ChangePinName(U2, 2, EMT_S)
+ChangePinName(U2, 1, COLL_S)
+
+# Start of element T1
+ChangePinName(T1, 3, 3)
+ChangePinName(T1, 4, 4)
+ChangePinName(T1, 1, 1)
+ChangePinName(T1, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 2, 2)
+ChangePinName(J1, 1, 1)
